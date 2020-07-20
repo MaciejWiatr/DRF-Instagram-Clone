7ac3122b4b99bad887623a52c794b4eefb92b386
Dettlaff <maciekwiatr17@gmail.com>
2020-07-20 11:40:50 +0200

    Upgrade user's profile serializer

--- accounts_api/serializers.py
+++ accounts_api/serializers.py
@@ -3,30 +3,39 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 
 
-class UserSerializer(serializers.ModelSerializer):
-    profile_id = serializers.PrimaryKeyRelatedField(
-        read_only=True, source="profile")
-
-    class Meta:
-        model = User
-        fields = ["id", "username", "email", "password", "profile_id"]
-
-        extra_kwargs = {
-            "password": {
-                "write_only": True,
-                "style": {"input_type": "password"}
-            }
-        }
-
-
 class UserProfileSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
     follows = serializers.PrimaryKeyRelatedField(
         many=True, queryset=UserProfile.objects.all())
     followed_by = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=UserProfile.objects.all())
+        many=True, read_only=True)
+    profile_id = serializers.IntegerField(source="id")
 
     class Meta:
         model = UserProfile
-        fields = ["id", "user", "age", "photo",
+        fields = ["profile_id", "user", "age", "photo",
                   "description", "follows", "followed_by"]
+
+        extra_kwargs = {
+            "followed_by": {
+                "read_only": True,
+            }
+        }
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "password", "profile"]
+
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "style": {"input_type": "password"}
+            },
+            "profile": {
+                "read_only": True
+            }
+        }
