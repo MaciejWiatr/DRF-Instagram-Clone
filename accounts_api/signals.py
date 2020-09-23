812150a4c67245a1f231dd2723a5944574c83c96
Maciej Wiatr <44440856+MaciejWiatr@users.noreply.github.com>
2020-09-23 18:40:53 +0200

    Update signals.py

--- accounts_api/signals.py
+++ accounts_api/signals.py
@@ -6,7 +6,6 @@ from .models import UserProfile
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-    print('siemanaoooo')
     if created:
         UserProfile.objects.create(user=instance)
     instance.profile.save()
