dbfce9295686089a52c1947fe193edbe3921f1db
Maciej Wiatr <maciekwiatr17@gmail.com>
2020-12-28 20:12:53 +0100

    Modify setup.bat
    From now on setup.bat will create dummy admin user

--- setup.bat
+++ setup.bat
@@ -3,3 +3,4 @@ python ./setupfiles/dotenv_setup.py
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
+python ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
